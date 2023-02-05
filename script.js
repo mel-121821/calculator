4121834951202b14df525b1d81955631794f623d
mel-121821 <97409605+mel-121821@users.noreply.github.com>
2023-02-05 09:32:30 -0500

    Create query selector variables for all buttons and both divs in the display

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 
+const btn1 = document.querySelector('#one');
+const btn2 = document.querySelector('#two');
+const btn3 = document.querySelector('#three');
+const btn4 = document.querySelector('#four');
+const btn5 = document.querySelector('#five');
+const btn6 = document.querySelector('#six');
+const btn7 = document.querySelector('#seven');
+const btn8 = document.querySelector('#eight');
+const btn9 = document.querySelector('#nine');
+const btn0 = document.querySelector('#zero');
+
+const btnAdd = document.querySelector('#add');
+const btnSubtract = document.querySelector('#subtract');
+const btnMultiply = document.querySelector('#multiply');
+const btnDivide = document.querySelector('#divide');
+
+const btnClear = document.querySelector('#clear');
+const btnBackspace = document.querySelector('#backspace');
+const btnEquals = document.querySelector('#equals');
+const btnDecimal = document.querySelector('#decimal');
+
 function add (a, b) {
     return a + b;    
 } 
