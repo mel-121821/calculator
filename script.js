ff1cb628257a509ce342b23029c5569187b1227c
mel-121821 <97409605+mel-121821@users.noreply.github.com>
2023-02-19 15:20:18 -0500

    Switch conversion of display values to be done within btnEquals event listener instead of an outside function (couldn't get it to work). Add operator function to btnEquals and test all values with console.log()

--- script.js
+++ script.js
@@ -93,16 +93,20 @@ btnClear.addEventListener('click', () => {
 });
 
 btnEquals.addEventListener('click', () => {
-    console.log(dispValA);
+    let a = Array.from(dispValA).join("");
+    a = parseInt(a);
+    console.log(a);
+    console.log(typeof(a));
     getDispValB();
-    console.log(dispValB);
+    let b = Array.from(dispValB).join("");
+    b = parseInt(b);
+    console.log(b);
+    console.log(typeof(b));
     console.log(operator);
-    let a = ConvArrToNum(dispValA);
-    console.log(a.joinArr());
-    console.log(typeof(a.joinArr()));
-    let b = ConvArrToNum(dispValB);
-    console.log(b.joinArr());
-    console.log(typeof(b.joinArr()));
+    operate(operator, a, b);
+    let mathResult = operate(operator, a, b);
+    console.log(mathResult);
+    
     
 
 })
@@ -142,7 +146,7 @@ function emptyDisplayValue() {
 
 let array = [5, 2, 4]
 
-function ConvArrToNum (value) {
+function convArrToNum (value) {
     return {
         joinArr: function() {
             value = Array.from(value).join("");
@@ -152,10 +156,24 @@ function ConvArrToNum (value) {
     }
 }
 
+//function convArrToNum (value) {
+    //value = Array.from(value).join("");
+    //console.log(value)
+   //value = parseInt(value);
+    //console.log(value)
+//};
+
+
+//convArrToNum(array);
+//console.log(array);
+
+    
+
+
 // used the Array.from method to convert value to an array, as it was considered by the computer as an object, not an array
 
 
-let v = ConvArrToNum(array);
+let v = convArrToNum(array);
 console.log(v.joinArr());
 console.log(typeof((v.joinArr())));
 
@@ -165,7 +183,7 @@ console.log(typeof((v.joinArr())));
 //console.log(convertArrayToNumber(array));
 
 function add (a, b) {
-    return a + b;    
+    return a + b;
 } 
 
 function subtract (a, b) {
@@ -181,6 +199,22 @@ function divide (a, b) {
 }
 
 
+//function operate (operator, a, b) {
+    //return {
+        //mathResult: function() {
+            //if (operator === "+" ) {
+                //return add(a, b);
+            //} if (operator === "-" ) {
+                //return subtract(a, b)
+            //} if (operator === "*" ) {
+                //return multiply(a, b)
+            //} if (operator === "/" ) {
+                //return divide(a, b)
+            //}
+        //}
+    //}
+//};
+
 function operate (operator, a, b) {
     if (operator === "+" ) {
         return add(a, b);
@@ -195,4 +229,6 @@ function operate (operator, a, b) {
 
 // An operator cannot be passed as a parameter to a  function, it must be passed as a string 
 
-console.log(operate("/", 2, 3));
+//let r = operate("+", 1, 9);
+//console.log(r.mathResult());
+
