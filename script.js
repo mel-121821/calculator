5e4bfa9787c341c07fe25356e9b1914796c075fa
mel-121821 <97409605+mel-121821@users.noreply.github.com>
2023-03-15 06:10:27 -0400

    Remove unused code, comments and console.logs

--- script.js
+++ script.js
@@ -4,11 +4,6 @@ const allNumBtns = document.querySelectorAll('.num');
 
 const allOperatorBtns = document.querySelectorAll('.operator');
 
-//const btnAdd = document.querySelector('#add');
-//const btnSubtract = document.querySelector('#subtract');
-//const btnMultiply = document.querySelector('#multiply');
-//const btnDivide = document.querySelector('#divide');
-
 const btnClear = document.querySelector('#clear');
 const btnBackspace = document.querySelector('#backspace');
 const btnEquals = document.querySelector('#equals');
@@ -52,88 +47,16 @@ for (btn of allOperatorBtns) {
 
 btnDecimal.addEventListener('click', () => {
     if (displayValue.includes('.') === true) {
-        //alert("Only one decimal per entry!");
         return null;
     } else {
     displayValue.push('.');
-    console.log(displayValue);
     result.textContent = displayValue.join("")
     }
 });
 
-
-
-
-//btnAdd.addEventListener('click', (e) => {
-    //if ((dispValA.length > 0) && (displayValue.length > 0 )) {
-        //performOperation();
-        //getDispValA();
-        //emptyDisplayValue();
-        //operator = e.target.textContent;
-        //userEntry.textContent = `${dispValA.join("")} ${operator}`
-    //} else {
-    //operator = e.target.textContent;
-    //getDispValA();
-    //userEntry.textContent = `${dispValA.join("")} ${operator}`
-    //emptyDisplayValue();
-    //result.textContent = displayValue;
-    //}
-//});
-
-//btnSubtract.addEventListener('click', (e) => {
-    //if ((dispValA.length > 0) && (displayValue.length > 0 )) {
-        //performOperation();
-        //getDispValA();
-        //emptyDisplayValue();
-        //operator = e.target.textContent;
-        //userEntry.textContent = `${dispValA.join("")} ${operator}`
-    //} else {
-    //operator = e.target.textContent;
-    //getDispValA();
-    //userEntry.textContent = `${dispValA.join("")} ${operator}`
-    //emptyDisplayValue();
-    //result.textContent = displayValue;
-    //}
-//});
-
-//btnMultiply.addEventListener('click', (e) => {
-    //if ((dispValA.length > 0) && (displayValue.length > 0 )) {
-        //performOperation();
-        //getDispValA();
-        //emptyDisplayValue();
-        //operator = e.target.textContent;
-        //userEntry.textContent = `${dispValA.join("")} ${operator}`
-    //} else {
-    //operator = e.target.textContent;
-    //getDispValA();
-    //userEntry.textContent = `${dispValA.join("")} ${operator}`
-    //emptyDisplayValue();
-    //result.textContent = displayValue;
-    //}
-//});
-
-//btnDivide.addEventListener('click', (e) => {
-    //if ((dispValA.length > 0) && (displayValue.length > 0 )) {
-        //console.log("Both dispValA and displayValue constain a value");
-        //performOperation();
-        //getDispValA();
-        //emptyDisplayValue();
-        //operator = e.target.textContent;
-        //userEntry.textContent = `${dispValA.join("")} $//{operator}`
-    //} else {
-    //operator = e.target.textContent;
-    //getDispValA();
-    //userEntry.textContent = `${dispValA.join("")} ${operator}`
-    //emptyDisplayValue();
-    //result.textContent = displayValue;
-    //}
-//});
-
-
 btnBackspace.addEventListener('click', () => {
     displayValue = displayValue.slice(0, -1)
     result.textContent = displayValue.join("");
-    console.log(displayValue);
 });
 
 btnClear.addEventListener('click', () => {
@@ -142,21 +65,12 @@ btnClear.addEventListener('click', () => {
     operator = "";
     userEntry.textContent = displayValue;
     result.textContent = displayValue;
-    console.log(displayValue);
-    console.log(dispValA);
-    console.log(operator);
 });
 
 btnEquals.addEventListener('click', () => {
-    console.log(`
-        displayValue = ${displayValue},
-        dispValA = ${dispValA}
-        dispValB = ${dispValB}
-        operator = ${operator}`)
     if ((dispValA.length > 0) && (displayValue.length > 0 )) {
     performOperation();
     userEntry.textContent = `${dispValA} ${operator} ${displayValue}`;
-    console.log(`At the end of operation, displayValue = ${displayValue}, dispvalA = ${dispValA}, dispValB = ${dispValB} and operator = ${operator}`);
     } else {
         return null;
     }
@@ -167,19 +81,13 @@ function performOperation () {
     getDispValB();
     let b = parseFloat(Array.from(dispValB).join(""));
     operate(operator, a, b);
-    console.log(a);
-    console.log(b);
     let mathResult = operate(operator, a, b);
-    console.log(mathResult);
     displayValue = [mathResult];
     userEntry.textContent = displayValue;
-    console.log(displayValue);
     dispValA = [];
     dispValB = [];
     operator = "";
-    console.log(`After performOperator is executed, operator = ${operator}`);
     result.textContent = dispValB;
-    console.log(`dispValA = ${dispValA}`)
 }
 
 // used the Array.from method to convert value to an array, as it was considered by the computer as an object, not an array
@@ -232,35 +140,4 @@ function operate (operator, a, b) {
 };
 
 // An operator cannot be passed as a parameter to a  function, it must be passed as a string 
-
-
-
-
-
-
-//function convertArrayToNumber(value) {
-    //value = value.join("");
-    //value = parseInt(value);
-    //return value;
-//}
-
-// Could not get the above or below functions to work, possibly due to scoping issue? See resource on variable scoping in README.md. May use this technique when I understand it better.
-
-//let array = [5, 2, 4]
-
-//function convArrToNum (value) {
-    //return {
-        //joinArr: function() {
-            //value = Array.from(value).join("");
-            //value = parseInt(value);
-            //return value;
-        //},
-    //}
-//}
-
-//let v = convArrToNum(array);
-//console.log(v.joinArr());
-//console.log(typeof((v.joinArr())));
-
-
 
