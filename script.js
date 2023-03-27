45133ba6343a9d2a66a222ad9e8c69579ae11bd7
mel-121821 <97409605+mel-121821@users.noreply.github.com>
2023-03-27 18:58:36 -0400

    Remove else statement on allOperatorBtns and add related comments. Remove console.logs()

--- script.js
+++ script.js
@@ -30,57 +30,33 @@ for (btn of allNumBtns) {
 
 // nested if statement not working (final else statement runs after execution of nested if statement and adds an unwanted operator). See Readme for resources, may switch to switch statement??
 
+//Solution: Added a condition to else statement to prevent code from running after alert message was clicked
+
 for (btn of allOperatorBtns) {
     btn.addEventListener('click', (e) => {
         if ((dispValA.length > 0) && (displayValue.length > 0 ) && (operator.length > 0)) {
-            //Check if dispValA already exists, if yes, use that, if no getDispValA from displayValue
-            console.log(`allOperatorBtns(2) before operation:
-            displayvalue = ${displayValue}
-            dispValA = ${dispValA}`)
             performOperation();
-            console.log(`allOperatorBtns(2) after operation:
-            displayvalue = ${displayValue}
-            dispValA = ${dispValA}`)
-            //getDispValA();
             emptyDisplayValue();
             operator = e.target.textContent;
             userEntry.textContent = `${dispValA.join("")} ${operator}`
         } if ((dispValA.length > 0) && (displayValue.length > 0 ) && (operator === "")) {
-            console.log(`btnAdd if statement(2):
-            operator = ${operator}`);
             alert("Please enter an operator");
             displayValue = [];
             dispValA = [];
             operator = "";
             userEntry.textContent = displayValue;
-            result.textContent = displayValue;
-            console.log(`btnAdd if statement (post execution):
-            displayValue = ${displayValue}
-            dispValA = ${dispValA}
-            dispValB = ${dispValB}
-            operator = ${operator}`);        
+            result.textContent = displayValue;       
         } if ((dispValA.length > 0) && (displayValue.length === 0)) {
-            console.log(`allOperatorBtns(3):
-            displayvalue = ${displayValue}
-            dispValA = ${dispValA}
-            operator = ${operator}`)
             operator = e.target.textContent;
-            //getDispValA();
             userEntry.textContent = `${dispValA.join("")} ${operator}`
-            //emptyDisplayValue();
             result.textContent = displayValue;
         } if ((dispValA.length === 0) && (displayValue.length > 0)) {
-        console.log(`allOperatorBtns(1):
-        displayvalue = ${displayValue}
-        dispValA = ${dispValA}
-        operator = ${operator}`)
-        operator = e.target.textContent;
-        getDispValA();
-        userEntry.textContent = `${dispValA.join("")} ${operator}`
-        emptyDisplayValue();
-        result.textContent = displayValue;
-        console.log(`operator = ${operator}`)
-        }
+            operator = e.target.textContent;
+            getDispValA();
+            userEntry.textContent = `${dispValA.join("")} ${operator}`
+            emptyDisplayValue();
+            result.textContent = displayValue;
+        } // Removed else statement because it would run after alert box is clicked
     });
 }
 
@@ -108,14 +84,8 @@ btnClear.addEventListener('click', () => {
 });
 
 btnEquals.addEventListener('click', () => {
-    console.log(`before btnEquals function runs:
-    displayValue = ${displayValue}
-    dispValA = ${dispValA}
-    dispValB = ${dispValB}
-    operator = ${operator}`);
     if ((dispValA.length > 0) && (displayValue.length > 0 )) {
     performOperation();
-    //userEntry.textContent = `${dispValA}`;
     } else {
         return null;
     }
@@ -125,39 +95,24 @@ btnEquals.addEventListener('click', () => {
 
 function performOperation () {
     if (operator === "") {
-        console.log(`performOperation if statement:
-        operator = ${operator}`);
         alert("Please enter an operator");
         displayValue = [];
         dispValA = [];
         operator = "";
         userEntry.textContent = displayValue;
         result.textContent = displayValue;
-        console.log(`performOperation if statement (post execution):
-        displayValue = ${displayValue}
-        dispValA = ${dispValA}
-        dispValB = ${dispValB}
-        operator = ${operator}`);
     } else {
-    let a = parseFloat(Array.from(dispValA).join(""));
-    getDispValB();
-    let b = parseFloat(Array.from(dispValB).join(""));
-    operate(operator, a, b);
-    let mathResult = operate(operator, a, b);
-    //displayValue = [mathResult];
-    dispValA = [mathResult];
-    //userEntry.textContent = displayValue;
-    userEntry.textContent = dispValA;
-    //dispValA = [];
-    displayValue = [];
-    dispValB = [];
-    operator = "";
-    result.textContent = dispValB;
-    console.log(`After performOperation:
-    displayValue = ${displayValue}
-    dispValA = ${dispValA}
-    dispValB = ${dispValB}
-    operator = ${operator}`);
+        let a = parseFloat(Array.from(dispValA).join(""));
+        getDispValB();
+        let b = parseFloat(Array.from(dispValB).join(""));
+        operate(operator, a, b);
+        let mathResult = operate(operator, a, b);
+        dispValA = [mathResult];
+        userEntry.textContent = dispValA;
+        displayValue = [];
+        dispValB = [];
+        operator = "";
+        result.textContent = dispValB;
     }}
 
 // used the Array.from method to convert value to an array, as it was considered by the computer as an object, not an array
